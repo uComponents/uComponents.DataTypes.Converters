e5b069d34b92de3217002b3b73c09ead21b006d1
Daniel Bardi <dbardi@gmail.com>
2013-12-27 03:31:55 -0700

    Added Map Coordinate Type Converter

---
+++ Src/uComponents.DataTypes.Converters.uSiteBuilder/Types/MapCoordinate.cs
@@ -0,0 +1,31 @@
+﻿namespace uComponents.DataTypes.Converters.uSiteBuilder.Types
+{
+    /// <summary>
+    /// </summary>
+    public class MapCoordinate
+    {
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        /// <value>
+        /// The longitude.
+        /// </value>
+        public float Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        /// <value>
+        /// The latitude.
+        /// </value>
+        public float Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the zoom.
+        /// </summary>
+        /// <value>
+        /// The zoom.
+        /// </value>
+        public int Zoom { get; set; }
+    }
+}
